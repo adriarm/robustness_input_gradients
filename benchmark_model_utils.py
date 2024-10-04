3f9020f29f40d352be9f30b69d88731728ef8e14
adriarm <adrianrm@mit.edu>
2024-10-04 13:48:04 -0400

    return eval mode

--- benchmark_model_utils.py
+++ benchmark_model_utils.py
@@ -57,4 +57,4 @@ def get_RodriguezMunoz2024Characterizing_model():
 
   normalize = NormalizeByChannelMeanStd(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
   model = torch.nn.Sequential(normalize, model)
-  return model
+  return model.eval()
