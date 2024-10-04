8f1c93cef84f5bb60abb50c63cdcf76105d41491
adriarm <adrianrm@mit.edu>
2024-10-04 13:41:07 -0400

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,15 +1,22 @@
 import torch
-
+import torch.nn as nn
 from robustbench import benchmark
-from myrobust model import MyRobustModel
+import torchvision.transforms as transforms
+from timm import create_model
+import swin_transformer_timm_version
+
+from benchmark_utils import get_Rodriguezmunoz2024Characterizing_model, get_data_transform
+
+transform = get_data_transform()
+model = get_Rodriguezmunoz2024Characterizing_model()
 
 threat_model = "Linf"  # one of {"Linf", "L2", "corruptions"}
 dataset = "imagenet"  # one of {"cifar10", "cifar100", "imagenet"}
 
-model = MyRobustModel()
-model_name = "<Name><Year><FirstWordOfTheTitle>"
+model_name = "Rodriguezmunoz2024Characterizing"
 device = torch.device("cuda:0")
 
 clean_acc, robust_acc = benchmark(model, model_name=model_name, n_examples=10000, dataset=dataset,
                                   threat_model=threat_model, eps=8/255, device=device,
+                                  preprocessing=transform, data_dir='/data/vision/torralba/datasets/imagenet_pytorch/train',
                                   to_disk=True)
---
+++ benchmark_utils.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from robustbench import benchmark
+import torchvision.transforms as transforms
+from timm import create_model
+
+import swin_transformer_timm_version
+
+def get_data_transform():
+  # Transform args
+  input_size = 244
+  crop_pct = 0.875
+  interpolation = 3
+
+  t = []
+  if input_size > 32:
+      size = int(input_size/crop_pct)
+      t.append(
+          transforms.Resize(size, interpolation=interpolation),
+      )
+      t.append(transforms.CenterCrop(input_size))
+  else:
+      t.append(
+          transforms.Resize(input_size, interpolation=interpolation),
+      )
+  t.append(transforms.ToTensor())
+  test_transform = transforms.Compose(t)
+  return test_transform
+
+def normalize_fn(tensor, mean, std):
+    """Differentiable version of torchvision.functional.normalize"""
+    # here we assume the color channel is in at dim=1
+    mean = mean[None, :, None, None]
+    std = std[None, :, None, None]
+    return tensor.sub(mean).div(std)
+
+class NormalizeByChannelMeanStd(nn.Module):
+    def __init__(self, mean, std):
+        super(NormalizeByChannelMeanStd, self).__init__()
+        if not isinstance(mean, torch.Tensor):
+            mean = torch.tensor(mean)
+        if not isinstance(std, torch.Tensor):
+            std = torch.tensor(std)
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
+
+    def forward(self, tensor):
+        return normalize_fn(tensor, self.mean, self.std)
+
+    def extra_repr(self):
+        return 'mean={}, std={}'.format(self.mean, self.std)
+
+def get_Rodriguezmunoz2024Characterizing_model():
+  model = create_model('swin_base_patch4_window7_224', pretrained=False, num_classes=1000, act_gelu=True)
+  ckpt=torch.load('gradnorm_swinb.pth.tar', map_location='cpu')
+  model.load_state_dict(ckpt['state_dict_ema'])
+
+  normalize = NormalizeByChannelMeanStd(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+  model = torch.nn.Sequential(normalize, model)
+  return model
