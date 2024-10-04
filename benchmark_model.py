d8e70e693358be3c5d3e4d58752dee1d2aecedb2
adriarm <adrianrm@mit.edu>
2024-10-04 13:46:00 -0400

    rename

--- benchmark_model.py
+++ benchmark_model.py
@@ -5,7 +5,7 @@ import torchvision.transforms as transforms
 from timm import create_model
 import swin_transformer_timm_version
 
-from benchmark_utils import get_RodriguezMunoz2024Characterizing_model, get_data_transform
+from benchmark_model_utils import get_RodriguezMunoz2024Characterizing_model, get_data_transform
 
 transform = get_data_transform()
 model = get_RodriguezMunoz2024Characterizing_model()
@@ -16,7 +16,7 @@ dataset = "imagenet"  # one of {"cifar10", "cifar100", "imagenet"}
 model_name = "RodriguezMunoz2024Characterizing"
 device = torch.device("cuda:0")
 
-clean_acc, robust_acc = benchmark(model, model_name=model_name, n_examples=10000, dataset=dataset,
+clean_acc, robust_acc = benchmark(model, model_name=model_name, n_examples=5000, dataset=dataset,
                                   threat_model=threat_model, eps=4./255., device=device,
-                                  preprocessing=transform, data_dir='/data/vision/torralba/datasets/imagenet_pytorch/train',
+                                  preprocessing=transform, data_dir='/data/vision/torralba/datasets/imagenet_pytorch',
                                   to_disk=True)
